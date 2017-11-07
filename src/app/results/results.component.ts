d6698b6b93e2c4c461797788f76a4792012c77dc
Kristen Kulha and Megan Olson <kristen@>
2017-11-07 11:54:03 -0800

    Attempt to add numbers to markers.

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -11,9 +11,8 @@ import { FirebaseListObservable } from 'angularfire2/database';
 })
 export class ResultsComponent {
   @Input() childRestaurants;
-
   zoom: number = 12;
-
+  numbers: string[] = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
   constructor() { }
 
 }
