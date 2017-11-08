829e17c56483c41573204a56d8f4f527094da3aa
Kristen Kulha and Megan Olson <kristen+megan@>
2017-11-08 10:45:58 -0800

    Resolve all conflicts.

--- src/app/user-auth.service.ts
+++ src/app/user-auth.service.ts
@@ -1,31 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from "rxjs/Observable";
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from "firebase/app";
+
 import { UserDetails } from './user-details.model';
+
 
 @Injectable()
 export class UserAuthService {
   user: Observable<firebase.User>;
-  users: FirebaseListObservable<any[]>;
-  currentUser;
+  userDetailList: FirebaseListObservable<any[]>;
+  createUserError: string;
 
-  constructor(public afAuth: AngularFireAuth, private database: AngularFireDatabase) {
+  constructor(public afAuth: AngularFireAuth, private router: Router, private database: AngularFireDatabase) {
     this.user = afAuth.authState;
-    this.users = database.list('users');
+    this.userDetailList = database.list("users");
   }
-
-  getUsers() {
-    return this.users;
-  }
-
-  // firebase.auth().currentUser.email
-  // this.database.object(users/0)
-  addToFavoritesList(chosenRestaurant) {
-    console.log(this.users);
-  }
-
 
   login() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
@@ -35,29 +27,41 @@ export class UserAuthService {
     this.afAuth.auth.signOut();
   }
 
+  addToFavoritesList(chosenRestaurant) {
+    console.log(this.userDetailList);
+  }
+
   createUser(
     email: string,
     password: string,
     firstName: string,
     lastName: string
   ) {
-    return firebase
+      firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .then(function(respond) {
+      .then((respond) => {
         let user = firebase.auth().currentUser;
-        return user.updateProfile({
+        console.log(user, "HELLO");
+        console.log(this.userDetailList, "hello");
+         user.updateProfile({
           displayName: firstName + " " + lastName,
           photoURL: ""
         });
+        let newUser: UserDetails = new UserDetails(email);
+        newUser.userId = user.uid;
+        this.addUser(newUser);
+
       })
       .then(() => firebase.auth().signOut())
-      .then(() => firebase.auth().signInWithEmailAndPassword(email, password))
-      .catch(function(error) {
-        let errorCode = error;
-        let errorMessage = error.message;
+      .catch((error) => {
+        this.createUserError = error.message;
       });
 
+  }
+
+  addUser(newUserDetails: UserDetails) {
+    this.userDetailList.push(newUserDetails);
   }
 
   signIn(
@@ -65,15 +69,15 @@ export class UserAuthService {
     password: string
   ) {
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      let errorCode = error;
-      let errorMessage = error.message;
+    .then((respond) => {
+      this.router.navigate([""]);
+    })
+    .catch((error) => {
+      this.createUserError = error.message;
     });
    }
 
    googleSignIn() {
        this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
      }
-
-
 }
