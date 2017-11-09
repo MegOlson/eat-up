3a96567da3582a4b4e026bd5586d12abea1de43e
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-08 19:24:28 -0800

    allow user to up load image

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,39 +1,32 @@
-import { Component, OnInit , Input} from '@angular/core';
-import { Router } from '@angular/router';
-import { UserDetails } from '../user-details.model';
-import { UserAuthService } from '../user-auth.service';
-import { FirebaseListObservable } from 'angularfire2/database';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { UserDetails } from "../user-details.model";
+import { UserAuthService } from "../user-auth.service";
+import { FirebaseListObservable } from "angularfire2/database";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
   providers: [UserAuthService]
 })
 export class DashboardComponent implements OnInit {
   favorites: FirebaseListObservable<any[]>;
   fullImagePath: string;
-  users: FirebaseListObservable<any[]>
+  users: FirebaseListObservable<any[]>;
   update: boolean = false;
   @Input() selectedUser;
   subUsers;
   currentUserImageUpdate;
 
-  constructor(private userAuthService: UserAuthService) { }
+  constructor(private userAuthService: UserAuthService) {}
 
   ngOnInit() {
     this.users = this.userAuthService.getUsers();
     this.users.subscribe(data => {
       this.subUsers = data;
-    })
-    this.fullImagePath = '../../assets/images/user.png';
-    console.log(this.currentUserImageUpdate);
-    // this.favorites = this.userAuthService.getFavorites();
-    if (this.currentUserImageUpdate.image.match(" ") ){
-    return this.fullImagePath = '../../assets/images/user.png';
-  } else {
-    return this.fullImagePath = '../../assets/images/' + this.currentUserImageUpdate.image;
-    }
+    });
+    this.fullImagePath = "../../assets/images/user.png";
   }
 
   removeItem(userToUpdate, favoriteToDelete) {
@@ -41,15 +34,46 @@ export class DashboardComponent implements OnInit {
     this.userAuthService.addToFavoritesList(userToUpdate);
   }
 
-  updateButtonClicked(){
+  updateButtonClicked() {
     this.update = true;
   }
 
-  savePhotoButtonClicked(image: string, userToUpdate: UserDetails){
-    this.update = false;
-    this.currentUserImageUpdate = userToUpdate;
-    userToUpdate.image = image.slice(12, image.length);
-    this.userAuthService.updateUserImage(userToUpdate);
-  }
+  savePhotoButtonClicked(image: any, userToUpdate: UserDetails) {
+    console.log(userToUpdate, "hello");
+    const canvas = document.createElement("canvas");
+    canvas.setAttribute("width", "200px");
+    canvas.setAttribute("height", "200px");
+    const context = canvas.getContext("2d");
 
+    if (image.files && image.files[0]) {
+      const FR = new FileReader();
+      FR.onload = (e: any) => {
+        const img = new Image();
+        img.addEventListener("load", () => {
+          context.drawImage(
+            img,
+            0,
+            0,
+            img.width,
+            img.height,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+          );
+          userToUpdate.image = canvas.toDataURL();
+          this.userAuthService.updateUserImage(userToUpdate);
+          this.currentUserImageUpdate = userToUpdate;
+        });
+        img.src = e.target.result;
+      };
+      FR.readAsDataURL(image.files[0]);
+    }
+    this.update = false;
+    if (!this.currentUserImageUpdate.image) {
+      return (this.fullImagePath = "../../assets/images/user.png");
+    } else {
+      return (this.fullImagePath = this.currentUserImageUpdate.image);
+    }
+  }
 }
--- src/app/user-auth.service.ts
+++ src/app/user-auth.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  AngularFireDatabase,
+  FirebaseListObservable
+} from "angularfire2/database";
 import { Observable } from "rxjs/Observable";
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from "firebase/app";
-import { UserDetails } from './user-details.model';
-
+import { UserDetails } from "./user-details.model";
 
 @Injectable()
 export class UserAuthService {
@@ -13,7 +15,11 @@ export class UserAuthService {
   userDetailList: FirebaseListObservable<any[]>;
   createUserError: string;
 
-  constructor(public afAuth: AngularFireAuth, private router: Router, private database: AngularFireDatabase) {
+  constructor(
+    public afAuth: AngularFireAuth,
+    private router: Router,
+    private database: AngularFireDatabase
+  ) {
     this.user = afAuth.authState;
     this.userDetailList = database.list("users");
   }
@@ -27,18 +33,19 @@ export class UserAuthService {
   }
 
   addToFavoritesList(userToUpdate) {
-    let userFromFirebase= this.getUserById(userToUpdate.$key);
-    userFromFirebase.update({ email: userToUpdate.email,
-                          favorites: userToUpdate.favorites,
-                          image: userToUpdate.image
+    let userFromFirebase = this.getUserById(userToUpdate.$key);
+    userFromFirebase.update({
+      email: userToUpdate.email,
+      favorites: userToUpdate.favorites,
+      image: userToUpdate.image
     });
   }
 
-  getUserById(userId: string){
-    return this.database.object('/users/' + userId);
+  getUserById(userId: string) {
+    return this.database.object("/users/" + userId);
   }
 
-  getUserEmail(){
+  getUserEmail() {
     return firebase.auth().currentUser.email;
   }
 
@@ -48,70 +55,68 @@ export class UserAuthService {
     firstName: string,
     lastName: string
   ) {
-      firebase
+    firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
-      .then((respond) => {
+      .then(respond => {
         let user = firebase.auth().currentUser;
-         user.updateProfile({
+        user.updateProfile({
           displayName: firstName + " " + lastName,
           photoURL: ""
         });
         let newUser: UserDetails = new UserDetails(email);
         newUser.userId = user.uid;
         this.addUser(newUser);
-
       })
       .then(() => firebase.auth().signOut())
-      .catch((error) => {
+      .catch(error => {
         this.createUserError = error.message;
       });
-
   }
 
   addUser(newUserDetails: UserDetails) {
     this.userDetailList.push(newUserDetails);
   }
 
-  signIn(
-    email: string,
-    password: string
-  ) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((respond) => {
-      this.router.navigate([""]);
-    })
-    .catch((error) => {
-      this.createUserError = error.message;
-    });
-   }
+  signIn(email: string, password: string) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(respond => {
+        this.router.navigate([""]);
+      })
+      .catch(error => {
+        this.createUserError = error.message;
+      });
+  }
 
-   googleSignIn() {
-       this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((result) => {
-         var token = result.credential.accessToken;
-         var googleUser = result.user;
+  googleSignIn() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(result => {
+        var token = result.credential.accessToken;
+        var googleUser = result.user;
 
-         let newUser: UserDetails = new UserDetails(googleUser.email);
-         let userList;
+        let newUser: UserDetails = new UserDetails(googleUser.email);
+        let userList;
 
-         this.getUsers().subscribe(data => {
-           userList = data;
-           for(let i = 0; i < userList; i++){
+        this.getUsers().subscribe(data => {
+          userList = data;
+          for (let i = 0; i < userList; i++) {
             if (userList[i].email === newUser.email) {
-              console.log("User is in database")
+              console.log("User is in database");
             } else {
               this.addUser(newUser);
             }
-           }
-         });
+          }
+        });
+      });
+  }
 
-       });
-     }
-
-     updateUserImage(localUpdateUser){
-       let userEntryInFirebase = this.getUserById(localUpdateUser.$key);
-       userEntryInFirebase.update({
-         image: localUpdateUser.image
-       });
-     }
+  updateUserImage(localUpdateUser) {
+    let userEntryInFirebase = this.getUserById(localUpdateUser.$key);
+    userEntryInFirebase.update({
+      image: localUpdateUser.image
+    });
+  }
 }
