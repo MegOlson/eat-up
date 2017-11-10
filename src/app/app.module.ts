6aa809418859a7453f0859dd846db969f3b99d59
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-10 12:29:39 -0800

    hide goolge api key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { routing } from './app.routing';
-import { masterFirebaseConfig } from './api-keys';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { SearchComponent } from './search/search.component';
-import { ResultsComponent } from './results/results.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { AgmCoreModule } from 'angular2-google-maps/core';
-import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
-import { UserPipe } from './user.pipe';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { routing } from "./app.routing";
+import { masterFirebaseConfig, googleMapsCore } from "./api-keys";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AppComponent } from "./app.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { SearchComponent } from "./search/search.component";
+import { ResultsComponent } from "./results/results.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { EditProfileComponent } from "./edit-profile/edit-profile.component";
+import { AgmCoreModule } from "angular2-google-maps/core";
+import { AgmSnazzyInfoWindowModule } from "@agm/snazzy-info-window";
+import { UserPipe } from "./user.pipe";
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -26,9 +26,9 @@ export const firebaseConfig = {
   storageBucket: masterFirebaseConfig.storageBucket
 };
 
-const googleMapsCore = AgmCoreModule.forRoot({
-  apiKey : 'AIzaSyDGSdrAlnmRxGQwhSe5y3oxUp5lxQr2s_I',
-});
+// const googleMapsCore = AgmCoreModule.forRoot({
+//   apiKey: "AIzaSyDGSdrAlnmRxGQwhSe5y3oxUp5lxQr2s_I"
+// });
 
 @NgModule({
   declarations: [
@@ -51,11 +51,11 @@ const googleMapsCore = AgmCoreModule.forRoot({
     routing,
     googleMapsCore,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDGSdrAlnmRxGQwhSe5y3oxUp5lxQr2s_I'
+      apiKey: "AIzaSyDGSdrAlnmRxGQwhSe5y3oxUp5lxQr2s_I"
     }),
     AgmSnazzyInfoWindowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/google-places.service.ts
+++ src/app/google-places.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class GooglePlacesService {
-
-  constructor(private http: Http) { }
+  constructor(private http: Http) {}
 
   getByCityAndType(city: string, foodType) {
-    return this.http.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + foodType + '+in+' + city + '&key=AIzaSyDGSdrAlnmRxGQwhSe5y3oxUp5lxQr2s_I')
+    return this.http.get(
+      "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
+        foodType +
+        "+in+" +
+        city +
+        "&key=AIzaSyDGSdrAlnmRxGQwhSe5y3oxUp5lxQr2s_I"
+    );
   }
-
 }
