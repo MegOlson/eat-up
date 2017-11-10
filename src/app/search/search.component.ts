f605ff719a0674326da04238071c6de1a3b1b961
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-10 10:09:00 -0800

    display error message if search input fields are not filled in

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
-import { UserDetails } from '../user-details.model';
-import { GooglePlacesService } from '../google-places.service';
-import { UserAuthService } from '../user-auth.service';
-import { Router } from '@angular/router';
-import { FirebaseListObservable } from 'angularfire2/database';
+import { Component } from "@angular/core";
+import { UserDetails } from "../user-details.model";
+import { GooglePlacesService } from "../google-places.service";
+import { UserAuthService } from "../user-auth.service";
+import { Router } from "@angular/router";
+import { FirebaseListObservable } from "angularfire2/database";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css'],
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
   providers: [GooglePlacesService]
 })
 export class SearchComponent {
   restaurants: any[];
-  users: FirebaseListObservable<any[]>
+  users: FirebaseListObservable<any[]>;
+  searchError: string;
 
-  constructor(private googlePlacesService: GooglePlacesService, private userAuthService: UserAuthService) { }
+  constructor(
+    private googlePlacesService: GooglePlacesService,
+    private userAuthService: UserAuthService
+  ) {}
 
   getRestaurants(city: string, foodType: string) {
-    this.googlePlacesService.getByCityAndType(city, foodType).subscribe(response => {
-      this.restaurants = response.json().results;
-    });
+    this.searchError = "";
+    if (city && foodType) {
+      this.googlePlacesService
+        .getByCityAndType(city, foodType)
+        .subscribe(response => {
+          this.restaurants = response.json().results;
+        });
+    } else {
+      if (!city && !foodType) {
+        this.searchError = "Please provide search options.";
+      } else if (!foodType) {
+        this.searchError = "Please provide food type.";
+      } else {
+        this.searchError = "Please provide city.";
+      }
+    }
   }
 }
