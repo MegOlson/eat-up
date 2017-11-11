515edae1a798f4cf7a7e97f97704458be4aea97f
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-10 16:54:24 -0800

    fix display error on new account page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  AngularFireDatabase,
+  FirebaseListObservable
+} from "angularfire2/database";
 import { AngularFireAuth } from "angularfire2/auth";
 import { Observable } from "rxjs/Observable";
 import * as firebase from "firebase/app";
 import { ActivatedRoute, Params } from "@angular/router";
 import { Location } from "@angular/common";
-import { UserDetails } from '../user-details.model';
-import { UserAuthService } from '../user-auth.service';
+import { UserDetails } from "../user-details.model";
+import { UserAuthService } from "../user-auth.service";
 
 @Component({
   selector: "app-signup",
@@ -15,7 +18,6 @@ import { UserAuthService } from '../user-auth.service';
   styleUrls: ["./signup.component.css"],
   providers: [UserAuthService, AngularFireAuth]
 })
-
 export class SignupComponent implements OnInit {
   user: Observable<firebase.User>;
   emailError: string;
@@ -30,7 +32,6 @@ export class SignupComponent implements OnInit {
     private router: Router
   ) {
     this.user = afAuth.authState;
-
   }
 
   ngOnInit() {}
@@ -42,44 +43,42 @@ export class SignupComponent implements OnInit {
     password: string,
     confirmPassword: string
   ) {
-    if(!this.checkEmail(email)){
-      this.emailError = "Invalid Email!"
-    } else if (!this.checkPassword(password, confirmPassword)){
-      this.passwordError = "Passwords do not match!"
-    }  else {
+    if (!this.checkEmail(email)) {
+      this.emailError = "Invalid Email!";
+    } else if (!this.checkPassword(password, confirmPassword)) {
+      this.passwordError = "Passwords do not match!";
+    } else {
       this.authService.createUser(email, password, firstName, lastName);
       if (this.signupAnyError) {
-      }else {
+        return this.signupAnyError;
+      } else {
         this.router.navigate([""]);
       }
     }
   }
 
-  checkEmail(
-    email: string,
-  ) {
-    if(email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
-      return true;
-    } else {
-      return false;
-    }
-   }
-
-  checkPassword(password: string, confirmPassword: string){
-    if(password === confirmPassword){
+  checkEmail(email: string) {
+    if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
       return true;
     } else {
       return false;
     }
   }
 
-  signupAnyError(){
-    if(this.authService.createUserError){
-      this.userError = this.authService.createUserError;
+  checkPassword(password: string, confirmPassword: string) {
+    if (password === confirmPassword) {
       return true;
     } else {
       return false;
     }
   }
 
+  signupAnyError() {
+    if (this.authService.returnCreateErrors()) {
+      this.userError = this.authService.returnCreateErrors();
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- src/app/user-auth.service.ts
+++ src/app/user-auth.service.ts
@@ -112,7 +112,7 @@ export class UserAuthService {
     });
   }
 
-  returnErrors(){
+  returnCreateErrors() {
     return this.createUserError;
   }
 }
